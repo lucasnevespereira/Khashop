b151d682df718c220a3beb26713573accf4d519a
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-02-15 17:51:49 +0100

    added paypal payments

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -6,6 +6,7 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
 } from "../constants/orderConstants";
 import axios from "axios";
