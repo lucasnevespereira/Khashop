45f5c439c6dfd5cbd3883281e7f7b3e8cbf0707a
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-02-20 18:12:09 +0100

    prepared for deployment

---
+++ frontend/build/precache-manifest.70d542466459121aebee90e8c607f5dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e489edc788e83f9b45a",
+    "url": "/static/css/main.75055205.chunk.css"
+  },
+  {
+    "revision": "6e489edc788e83f9b45a",
+    "url": "/static/js/main.6e489edc.chunk.js"
+  },
+  {
+    "revision": "418d8d3252a798389714",
+    "url": "/static/js/1.418d8d32.chunk.js"
+  },
+  {
+    "revision": "c11ae9018cc319446375",
+    "url": "/static/js/2.c11ae901.chunk.js"
+  },
+  {
+    "revision": "9883271a063697fc32be",
+    "url": "/static/js/runtime~main.9883271a.js"
+  },
+  {
+    "revision": "151ab47deff96cd6106093d92048acb0",
+    "url": "/index.html"
+  }
+];
